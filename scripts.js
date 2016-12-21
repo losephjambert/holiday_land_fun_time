9190cd147c6c7f122dbbae660aaf8afe554e05e1
Joseph Lambert <josephc.lambert@gmail.com>
2016-12-20 22:42:17 -0800

    fixing snow canvas element on mobile

--- scripts.js
+++ scripts.js
@@ -1,55 +1,58 @@
-var c = document.getElementById('snow'), 
-    $ = c.getContext("2d");
-var w = c.width = window.innerWidth, 
-    h = c.height = window.innerHeight;
+window.onload = function(){
+  //create canvas
+  var canvas = document.getElementById("snow");
+  var ctx = canvas.getContext("2d");
+  
+  //set canvas fullscreen
+  var W = window.innerWidth;
+  var H = window.innerHeight;
+  canvas.height = H;
+  canvas.width = W;
+  
+  //generate snowflakes and atts
+  var mf = 200; //max flakes
+  var flakes = [];
 
-Snowy();
-function Snowy() {
-  var snow, arr = [];
-  var num = 600, tsc = 1, sp = 1;
-  var sc = 1.3, t = 0, mv = 20, min = 1;
-    for (var i = 0; i < num; ++i) {
-      snow = new Flake();
-      snow.y = Math.random() * (h + 50);
-      snow.x = Math.random() * w;
-      snow.t = Math.random() * (Math.PI * 2);
-      snow.sz = (100 / (10 + (Math.random() * 100))) * sc;
-      snow.sp = (Math.pow(snow.sz * .8, 2) * .15) * sp;
-      snow.sp = snow.sp < min ? min : snow.sp;
-      arr.push(snow);
-    }
-  go();
-  function go(){
-    window.requestAnimationFrame(go);
-      $.clearRect(0, 0, w, h);
-      $.fillStyle = 'hsla(242, 95%, 3%, 1)';
-      $.fillRect(0, 0, w, h);
-      $.fill();
-        for (var i = 0; i < arr.length; ++i) {
-          f = arr[i];
-          f.t += .05;
-          f.t = f.t >= Math.PI * 2 ? 0 : f.t;
-          f.y += f.sp;
-          f.x += Math.sin(f.t * tsc) * (f.sz * .3);
-          if (f.y > h + 50) f.y = -10 - Math.random() * mv;
-          if (f.x > w + mv) f.x = - mv;
-          if (f.x < - mv) f.x = w + mv;
-          f.draw();}
- }
- function Flake() {
-   this.draw = function() {
-      this.g = $.createRadialGradient(this.x, this.y, 0, this.x, this.y, this.sz);
-      this.g.addColorStop(0, 'hsla(255,255%,255%,1)');
-      this.g.addColorStop(1, 'hsla(255,255%,255%,0)');
-      $.moveTo(this.x, this.y);
-      $.fillStyle = this.g;
-      $.beginPath();
-      $.arc(this.x, this.y, this.sz, 0, Math.PI * 2, true);
-      $.fill();}
+  
+  //loop through empty flakes and apply atts
+for(var i = 0; i < mf; i++){
+    flakes.push({
+      x: Math.random()*W, //set width of flake to random nr between 0 and 1 * width of screen
+      y: Math.random()*H, //set height of flake to random nr between 0 and 1 * height of screen
+      r: Math.random()*1+2, //set radius between 2 and 5
+      d: Math.random() + 1
+    })
+
   }
+
+
+  //draw flakes
+  function drawFlakes(){
+    ctx.clearRect(0, 0, W, H);
+    ctx.fillStyle = "White";
+    ctx.beginPath();
+    for(var i = 0; i < mf; i++){
+      var f = flakes[i];
+      ctx.moveTo(f.x, f.y);
+      ctx.arc(f.x, f.y, f.r, 0, Math.PI*2, true);
+    }
+    ctx.fill();
+    moveFlakes();
+  }
+  var angle = 0;
+  //move flakes
+  function moveFlakes(){
+    angle += 0.01;
+    for(var i = 0; i < mf; i++){
+      var f = flakes[i];
+      f.y += Math.pow(f.d, 2) + 1;
+      f.x += Math.cos(angle)*2;
+      
+      if(f.y > H){
+        flakes[i] = {x: Math.random()*W, y: 0, r: f.r, d: f.d};
+      }
+    }
+  }
+  
+  setInterval(drawFlakes, 25);
 }
-/*________________________________________*/
-window.addEventListener('resize', function(){
-  c.width = w = window.innerWidth;
-  c.height = h = window.innerHeight;
-}, false);
