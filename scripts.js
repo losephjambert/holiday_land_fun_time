2df0203de66a6b6a283de791114b20a2919fdc15
Joseph Lambert <josephc.lambert@gmail.com>
2016-12-20 22:12:03 -0800

    updating snow mechanic

--- scripts.js
+++ scripts.js
@@ -1,74 +1,55 @@
-var canvas = document.getElementById('snow'),
-    ctx = canvas.getContext('2d'),
-    width = ctx.canvas.width = document.body.offsetWidth,
-    height = ctx.canvas.height = document.body.offsetHeight,
-    animFrame = window.requestAnimationFrame ||
-                window.mozRequestAnimationFrame ||
-                window.webkitRequestAnimationFrame ||
-                window.msRequestAnimationFrame,
-    snowflakes = [];
+var c = document.getElementById('snow'), 
+    $ = c.getContext("2d");
+var w = c.width = window.innerWidth, 
+    h = c.height = window.innerHeight;
 
-window.onresize = function() {
-  width = ctx.canvas.width = document.body.offsetWidth,
-    height = ctx.canvas.height = document.body.offsetHeight;
-}
-
-function update() {
-  for (var i = 0; i < snowflakes.length; i++) {
-    snowflakes[i].update();
+Snowy();
+function Snowy() {
+  var snow, arr = [];
+  var num = 600, tsc = 1, sp = 1;
+  var sc = 1.3, t = 0, mv = 20, min = 1;
+    for (var i = 0; i < num; ++i) {
+      snow = new Flake();
+      snow.y = Math.random() * (h + 50);
+      snow.x = Math.random() * w;
+      snow.t = Math.random() * (Math.PI * 2);
+      snow.sz = (100 / (10 + (Math.random() * 100))) * sc;
+      snow.sp = (Math.pow(snow.sz * .8, 2) * .15) * sp;
+      snow.sp = snow.sp < min ? min : snow.sp;
+      arr.push(snow);
+    }
+  go();
+  function go(){
+    window.requestAnimationFrame(go);
+      $.clearRect(0, 0, w, h);
+      $.fillStyle = 'hsla(242, 95%, 3%, 1)';
+      $.fillRect(0, 0, w, h);
+      $.fill();
+        for (var i = 0; i < arr.length; ++i) {
+          f = arr[i];
+          f.t += .05;
+          f.t = f.t >= Math.PI * 2 ? 0 : f.t;
+          f.y += f.sp;
+          f.x += Math.sin(f.t * tsc) * (f.sz * .3);
+          if (f.y > h + 50) f.y = -10 - Math.random() * mv;
+          if (f.x > w + mv) f.x = - mv;
+          if (f.x < - mv) f.x = w + mv;
+          f.draw();}
+ }
+ function Flake() {
+   this.draw = function() {
+      this.g = $.createRadialGradient(this.x, this.y, 0, this.x, this.y, this.sz);
+      this.g.addColorStop(0, 'hsla(255,255%,255%,1)');
+      this.g.addColorStop(1, 'hsla(255,255%,255%,0)');
+      $.moveTo(this.x, this.y);
+      $.fillStyle = this.g;
+      $.beginPath();
+      $.arc(this.x, this.y, this.sz, 0, Math.PI * 2, true);
+      $.fill();}
   }
 }
-
-function Snow() {
-  this.x = random(0, width);
-  this.y = random(-height, 0);
-  this.radius = random(0.5, 3.0);
-  this.speed = random(1, 10);
-  this.wind = random(-0.5, 3.0);
-}
-
-Snow.prototype.draw = function() {
-  ctx.beginPath();
-  ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-  ctx.fillStyle = '#FEF0ED';
-  ctx.fill();
-  ctx.closePath();
-}
-
-Snow.prototype.update = function() {
-  this.y += this.speed;
-  this.x += this.wind;
-
-  if (this.y > ctx.canvas.height) {
-    this.y = 0;
-    this.x = random(0, width);
-  }
-}
-
-function createSnow(count) {
-  for (var i = 0; i < count; i++) {
-    snowflakes[i] = new Snow();
-  }
-}
-
-function draw() {
-  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-  for (var i = 0; i < snowflakes.length; i++) {
-    snowflakes[i].draw();
-  }
-}
-
-function loop() {
-  draw();
-  update();
-  animFrame(loop);
-}
-
-function random(min, max) {
-  var rand = (min + Math.random() * (max - min)).toFixed(1);
-  rand = Math.round(rand);
-  return rand;
-}
-
-createSnow(150);
-loop();
+/*________________________________________*/
+window.addEventListener('resize', function(){
+  c.width = w = window.innerWidth;
+  c.height = h = window.innerHeight;
+}, false);
